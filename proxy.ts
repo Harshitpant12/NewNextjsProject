43ce22eb86d0273307c6977b5a8da2cb4f159a2b
Harshitpant12 <harshpant778@gmail.com>
2026-04-26 09:37:39 +0530

    implement middleware or proxy setup

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,41 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server';
 
-export default clerkMiddleware()
+const isPublicRoute = createRouteMatcher([
+    "/signin",
+    "/signup",
+    "/",
+    "/home"
+])
+
+const isPublicApiRoute = createRouteMatcher([
+    "/api/videos"
+])
+
+export default clerkMiddleware(async (auth, req) => {
+  const { userId } = await auth();
+  const currentUrl = new URL(req.url)
+  const isAccessingDashboard = currentUrl.pathname === "/home"
+  const isApiRequest = currentUrl.pathname.startsWith("/api")
+
+  if(userId && isPublicRoute(req) && !isAccessingDashboard){
+    return NextResponse.redirect(new URL ("/home", req.url))
+  }
+
+  // not logged in
+  if(!userId) {
+    // if the user is trying to access a protected route without login
+    if(!isPublicRoute(req) && !isPublicApiRoute(req)){
+      return NextResponse.redirect(new URL ("/signin", req.url))
+    }
+
+    // if the request is for protected api and the user is not logged in
+    if(isApiRequest && !isPublicApiRoute(req)) {
+      return NextResponse.redirect(new URL ("/signin", req.url))
+    }
+  }
+  return NextResponse.next() // middleware next function
+})
 
 export const config = {
   matcher: [
