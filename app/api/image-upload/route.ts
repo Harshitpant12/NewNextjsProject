ea12dfce4b90550f297f1fc5fcb9a46931ae357e
Harshitpant12 <harshpant778@gmail.com>
2026-05-01 11:23:23 +0530

    implement cloudinary file uploading logic for image upload route

--- app/api/image-upload/route.ts
+++ app/api/image-upload/route.ts
@@ -1,7 +1,6 @@
 import { NextRequest, NextResponse } from "next/server"
 import { v2 as cloudinary } from "cloudinary"
 import { auth } from "@clerk/nextjs/server"
-import { error } from "console";
 
 // configuration
 
@@ -20,5 +19,33 @@ export async function POST(request: NextRequest) {
     const {userId} = await auth()
     if(!userId){
         return NextResponse.json({error: "Unauthorized"}, {status: 401})
+    }
+
+    try {
+        const formData = await request.formData()
+        const file = formData.get('file') as File | null
+
+        if(!file) return NextResponse.json({ error: "File Not Found!" }, {status: 400})
+
+        const bytes = await file.arrayBuffer() // converting it as arraybuffer
+        const buffer = Buffer.from(bytes) // create a buffer from that arraybuffer
+
+        const result = await new Promise<CloudinaryUploadResult>(
+            (resolve, reject) => {
+                const uploadStream = cloudinary.uploader.upload_stream(
+                    {folder: "next-cloudinary-uploads"},
+                    (error, result) => {
+                        if(error) reject(error);
+                        else resolve(result as CloudinaryUploadResult)
+                    }
+                )
+                uploadStream.end(buffer)
+            }
+        )
+
+        return NextResponse.json({publicId: result.public_id}, {status: 200})
+    } catch (error) {
+        console.log("Upload image failed", error);
+        return NextResponse.json({error: "Upload image failed"}, {status: 500})
     }
 }
