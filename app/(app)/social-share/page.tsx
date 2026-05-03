9d417f63ea5e405521db020144f65766c31e61e5
Harshitpant12 <harshpant778@gmail.com>
2026-05-03 16:41:23 +0530

    implement functionality to handle download of images

--- app/(app)/social-share/page.tsx
+++ app/(app)/social-share/page.tsx
@@ -50,6 +50,23 @@ export default function SocialShare() {
       setIsUploading(false)
     }
   }
+
+  const handleDownload = () => {
+    if(!imageRef.current) return;
+
+    fetch(imageRef.current.src)
+    .then((response) => response.blob())
+    .then((blob) => {
+      const url = window.URL.createObjectURL(blob)
+      const link = document.createElement("a")
+      link.href = url;
+      link.download = `${selectedFormat.replace(/\s+/g, "_").toLowerCase()}.png`
+      document.body.appendChild(link)
+      link.click()
+      document.body.removeChild(link)
+      window.URL.revokeObjectURL(url)
+    })
+  }
   return (
     <div>SocialShare</div>
   )
