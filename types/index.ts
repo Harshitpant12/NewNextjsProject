3b4a8769496c2cfea88b02ebedc5384ad1018041
Harshitpant12 <harshpant778@gmail.com>
2026-05-04 16:27:42 +0530

    add types, update the home page ui and functionalities

---
+++ types/index.ts
@@ -0,0 +1,11 @@
+export interface Video {
+    id: string
+    title: string
+    description: string
+    publicId: string
+    originalSize: string
+    compressedSize: string
+    duration: string
+    createdAt: Date
+    updatedAt: Date
+}
